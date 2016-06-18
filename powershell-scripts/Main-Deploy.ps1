5572fe5a9110cf9ab5140dcac683bd470e70c23e
farthir <rfarthing@outlook.com>
2016-06-19 00:51:27 +0100

    remove api on extension

--- powershell-scripts/Main-Deploy.ps1
+++ powershell-scripts/Main-Deploy.ps1
@@ -5,5 +5,5 @@
 cd '~\Source\Repos\azure-scalable-iaas\powershell-scripts'
 $resourceGroupLocation = "northeurope"
 
-.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName "azure-scalable-iaas" -Environment "test"
+.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName "azure-scalable-iaas_test" -Environment "test"
 .\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName "azure-scalable-iaas" -Environment "prod"
