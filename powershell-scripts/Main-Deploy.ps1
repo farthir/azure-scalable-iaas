8b2736d56593f5bbd6fdfbcda1cae0296b187082
farthir <rfarthing@outlook.com>
2016-06-19 01:42:58 +0100

    add load balancing rules and probes

--- powershell-scripts/Main-Deploy.ps1
+++ powershell-scripts/Main-Deploy.ps1
@@ -6,4 +6,4 @@ cd '~\Source\Repos\azure-scalable-iaas\powershell-scripts'
 $resourceGroupLocation = "northeurope"
 
 .\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName "azure-scalable-iaas_test" -Environment "test"
-.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName "azure-scalable-iaas" -Environment "prod"
+.\Deploy-AzureResourceGroup.ps1 -ResourceGroupLocation $resourceGroupLocation -ResourceGroupName "azure-scalable-iaas_prod" -Environment "prod"
