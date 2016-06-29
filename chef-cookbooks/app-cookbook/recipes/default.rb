f52736f4ff0160df51b98632b908f0fa28f78c05
farthir <rfarthing@outlook.com>
2016-06-29 01:51:17 +0100

    standardise param files, adjust permissions on app server

--- chef-cookbooks/app-cookbook/recipes/default.rb
+++ chef-cookbooks/app-cookbook/recipes/default.rb
@@ -10,22 +10,22 @@ tomcat_install 'companynews' do
   version '8.5.3'
 end
 
-#remote_file '/opt/tomcat_companynews_8_5_3/webapps/companyNews.war' do
-#  owner 'tomcat_companynews'
-#  mode '0644'
-#  source "#{node['app-cookbook']['war-source']}"
-#  action :create
-#end
+remote_file '/opt/tomcat_companynews/webapps/companyNews.war' do
+  owner 'tomcat_companynews'
+  mode '0644'
+  source "#{node['app-cookbook']['war-source']}"
+  action :create
+end
 
-#directory '/Users/dcameron/persistence/files*' do
-#  owner 'tomcat_companynews'
-#  group 'tomcat_companynews'
-#  mode '0777'
-#  action :create
-#  recursive true
-#end
+directory '/Users/dcameron/persistence/files*' do
+  owner 'tomcat_companynews'
+  group 'tomcat_companynews'
+  mode '0774'
+  action :create
+  recursive true
+end
 
-#tomcat_service 'companynews' do
-#  action [:start, :enable]
-#  sensitive true
-#end
+tomcat_service 'companynews' do
+  action [:start, :enable]
+  sensitive true
+end
