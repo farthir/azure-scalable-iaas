f1068b7e131db1b5a78923ce30b4155f3c68d450
Rob Farthing <rofarthi@microsoft.com>
2016-07-20 18:34:15 -0700

    prep for DevOps demo

--- cookbooks/app-cookbook/attributes/default.rb
+++ cookbooks/app-cookbook/attributes/default.rb
@@ -1,6 +1,6 @@
 
 # specify later java version than default 6
 default['java']['jdk_version'] = '7'
-default['app-cookbook']['war-source'] = 'https://twconf.blob.core.windows.net/artefacts/companyNews.war?st=2016-06-29T00%3A32%3A00Z&se=2017-06-30T00%3A32%3A00Z&sp=rl&sv=2015-04-05&sr=b&sig=KoC%2FddCqw3BaJny2zCOpo9Pm3ECxetRk0oN9OjkIZ7Q%3D'
-default['app-cookbook']['war-name'] = 'companyNews'
-default['app-cookbook']['prevayler-dir'] = '/Users/dcameron/persistence/files*'
+default['app-cookbook']['war-source'] = 'https://tr23devopsconf.blob.core.windows.net/conf/sample.war'
+default['app-cookbook']['war-name'] = 'sample'
+default['app-cookbook']['prevayler-dir'] = '/prevayler/temp'
--- cookbooks/web-cookbook/attributes/default.rb
+++ cookbooks/web-cookbook/attributes/default.rb
@@ -1,4 +1,4 @@
 
-default['web-cookbook']['zip-source'] = 'https://twconf.blob.core.windows.net/artefacts/static.zip?st=2016-06-29T00%3A32%3A00Z&se=2017-06-30T00%3A32%3A00Z&sp=rl&sv=2015-04-05&sr=b&sig=zLjnj5ghbs7hI%2FYqXzNzOhCWZUDkKZYMbC5MZxO1r9g%3D'
+default['web-cookbook']['zip-source'] = 'https://tr23devopsconf.blob.core.windows.net/conf/static.zip'
 default['web-cookbook']['app-lb-ip'] = '10.0.0.4'
-default['web-cookbook']['static-dir-name'] = 'companyNews'
+default['web-cookbook']['static-dir-name'] = 'sample'
