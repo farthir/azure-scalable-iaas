06a1c7a19f9dae030d7319d5d83b0d73c03edbe4
Rob Farthing <rofarthi@microsoft.com>
2016-07-26 20:30:26 -0700

    scale up app tier and serve gif files from nginx

--- cookbooks/web-cookbook/metadata.rb
+++ cookbooks/web-cookbook/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures web-cookbook'
 long_description 'Installs/Configures web-cookbook'
-version '0.6.1'
+version '0.6.2'
 
 depends 'nginx'
